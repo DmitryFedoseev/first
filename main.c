b12377f3fd675d09e6453c7c85ecfaa33ecbe904
Dmitry <feddiman02@gmail.com>
2020-10-26 00:18:10 +0300

    Second commit

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int  main(){
-    printf("Hello, World!");
+    printf("Hello, World!\n");
+    int a, b, c;
+    a = 5;
+    b = 2;
+    c = a + b;
+    printf("%d", c);
     return 0;
 }
